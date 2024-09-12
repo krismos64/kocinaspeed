296a4199cc70a2f5bbdcb24df50497f344ae1b9c
krismos64 <c.mostefaoui@yahoo.fr>
2024-09-12 12:18:35 +0200

    Connexion button and form login added

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security as SecurityBundleSecurity;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,8 +11,17 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SecurityBundleSecurity $security): Response
     {
+        // Si l'utilisateur est déjà connecté, on redirige vers l'admin ou la page d'accueil selon son rôle
+        if ($this->getUser()) {
+            if ($security->isGranted('ROLE_ADMIN')) {
+                return $this->redirectToRoute('app_admin');
+            } else {
+                return $this->redirectToRoute('app_home');
+            }
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -21,6 +31,13 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function admin()
+    {
+        // Page d'administration, accessible uniquement pour les administrateurs
+        return $this->render('admin/dashboard.html.twig');
     }
 
     #[Route('/logout', name: 'app_logout')]
